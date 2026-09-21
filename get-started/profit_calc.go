9a25d2569a0fb539351786cf87b798ce3b1c525f
mlo-dev <mlondiemchunu1@gmail.com>
2026-09-21 23:09:35 +0200

    profit calc edits

--- get-started/profit_calc.go
+++ get-started/profit_calc.go
@@ -8,13 +8,13 @@ func main() {
 	var revenue, expenses, taxRate float64
 
 	fmt.Print(`revenue : `)
-	fmt.Scan(revenue)
+	fmt.Scan(&revenue)
 
 	fmt.Print(`expenses : `)
-	fmt.Scan(expenses)
+	fmt.Scan(&expenses)
 
 	fmt.Print(`tax rate :`)
-	fmt.Scan(taxRate)
+	fmt.Scan(&taxRate)
 
 	ebt := revenue - expenses
 	profit := ebt * (1 - taxRate/100)
